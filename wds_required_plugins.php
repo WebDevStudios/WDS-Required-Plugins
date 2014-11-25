1b14d2a0a0a4c9e854fbc56fe5628e7c1456b265
Justin Sternberg <justin@dsgnwrks.pro>
2014-11-25 16:01:33 -0500

    Add translation stuff

--- wds_required_plugins.php
+++ wds_required_plugins.php
@@ -6,7 +6,9 @@ Description: Make certain plugins required so that they cannot be (easily) deact
 Author: WebDevStudios
 Author URI: http://webdevstudios.com
 Version: 0.1.1
+Domain: wds-required-plugins
 License: GPLv2
+Path: languages
 */
 
 // Exit if accessed directly
@@ -31,6 +33,12 @@ class WDS_Required_Plugins {
 	public static $instance = null;
 
 	/**
+	 * Whether text-domain has been registered
+	 * @var boolean
+	 */
+	private static $l10n_done = false;
+
+	/**
 	 * Creates or returns an instance of this class.
 	 * @since  0.1.0
 	 * @return WDS_Required_Plugins A single instance of this class.
@@ -51,8 +59,9 @@ class WDS_Required_Plugins {
 	private function __construct() {
 		add_filter( 'admin_init', array( $this, 'activate_if_not' ) );
 		add_filter( 'plugin_action_links', array( $this, 'filter_plugin_links' ), 10, 2 );
+		// load text domain
+		add_action( 'plugins_loaded', array( $this, 'l10n' ) );
 	}
-
 
 	/**
 	 * Activate required plugins if they are not.
@@ -66,7 +75,6 @@ class WDS_Required_Plugins {
 			}
 		}
 	}
-
 
 	/**
 	 * Remove the deactivation link for all custom/required plugins
@@ -88,12 +96,11 @@ class WDS_Required_Plugins {
 
 		// Remove deactivate link for required plugins
 		if( array_key_exists( 'deactivate', $actions ) && in_array( $plugin_file, $this->get_required_plugins() ) ) {
-			$actions['deactivate'] = sprintf( '<span style="color: #888">%s</span>', __( 'WDS Required Plugin', '_s' ) );
+			$actions['deactivate'] = sprintf( '<span style="color: #888">%s</span>', __( 'WDS Required Plugin', 'wds-required-plugins' ) );
 		}
 
 		return $actions;
 	}
-
 
 	/**
 	 * Get the plugins that are required for the project. Plugins will be registered by the wds_required_plugins filter
@@ -104,6 +111,31 @@ class WDS_Required_Plugins {
 	 */
 	public function get_required_plugins() {
 		return (array) apply_filters( 'wds_required_plugins', array() );
+	}
+
+	/**
+	 * Load this library's text domain
+	 * @since  0.2.1
+	 */
+	public function l10n() {
+		// Only do this one time
+		if ( self::$l10n_done ) {
+			return;
+		}
+
+		$loaded = load_plugin_textdomain( 'wds-required-plugins', false, '/languages/' );
+		if ( ! $loaded ) {
+			$loaded = load_muplugin_textdomain( 'wds-required-plugins', '/languages/' );
+		}
+		if ( ! $loaded ) {
+			$loaded = load_theme_textdomain( 'wds-required-plugins', '/languages/' );
+		}
+
+		if ( ! $loaded ) {
+			$locale = apply_filters( 'plugin_locale', get_locale(), 'wds-required-plugins' );
+			$mofile = dirname( __FILE__ ) . '/languages/wds-required-plugins-'. $locale .'.mo';
+			load_textdomain( 'wds-required-plugins', $mofile );
+		}
 	}
 
 }
